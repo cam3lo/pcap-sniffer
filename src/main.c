8b5cd762f7cefe9c86aeef8c2482ac471375f393
Jese Camilo <jese.r.camilo@gmail.com>
2018-09-15 15:08:49 -0400

    done

--- src/main.c
+++ src/main.c
@@ -10,11 +10,19 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+void callback(u_char *useless,
+        const struct pcap_pkthdr *pkthdr,
+        const u_char *packet)
+{
+  static int count = 1;
+  printf("\nPacket number [%d], length of this packet is: %d\n", count++, pkthdr->len);
+}
+
 int main(int argc, char *argv[]) {
     char *dev, dev_buff[64] = { 0 }, errbuf[PCAP_ERRBUF_SIZE];
     int i = 0;
     pcap_t *descr;
-    struct bpf_program fp; // hold compiled program
+    struct bpf_program fp; // holds compiled program
     bpf_u_int32 pMask;     // subnet mask
     bpf_u_int32 pNet;      // ip address
     pcap_if_t *alldevs, *device;
@@ -38,8 +46,57 @@ int main(int argc, char *argv[]) {
     for(device = alldevs; device; device = device->next) {
         printf("%d. %s", ++i, device->name);
         if(device->description)
-            printf("\t(%s)\n", device->description);
+            printf("\t\t%s\n", device->description);
         else
-            printf("\tSorry, no description available for this device)\n");
+            printf("\t\t(Sorry, no description available for this device)\n");
     }
+
+    // ask user to specify interface for sniffing
+    printf("\nEnter the interface for sniffing: ");
+    fgets(dev_buff, sizeof(dev_buff) - 1, stdin);
+
+    dev_buff[strlen(dev_buff) - 1] = '\0'; // remove trailing newline
+
+    if(strlen(dev_buff)) {
+        dev = dev_buff;
+        if(dev == NULL) {
+            printf("\n[%s]\n", errbuf);
+            exit(1);
+        } else{
+            printf("\n---You chose device [%s] to capture [%d] packets---\n");
+        }
+    } else {
+        printf("\n---Invalid Entry---\n");
+        exit(1);
+    }
+
+    // fetch network address and network mask
+    pcap_lookupnet(dev, &pNet, &pMask, errbuf);
+
+    // open device for sniffing
+    descr = pcap_open_live(dev, BUFSIZ, 0,-1, errbuf);
+    if(descr == NULL) {
+        printf("\npcap_open_live() failed due to:\n[%s]\n", errbuf);
+        return -1;
+    }
+
+    // compile the filter expression
+    if(pcap_compile(descr, &fp, argv[1], 0, pNet) == -1) {
+        printf("\npcap_compile() failed\n");
+        return -1;
+    }
+
+    // filter compiled expression
+    if(pcap_setfilter(descr, &fp) == -1) {
+        printf("\npcap_setfilter() failed\n");
+        exit(1);
+    }
+
+    // for every packet received, call the callback function
+    // for now maximum limit on number of packets is specified
+    // by user.
+    pcap_loop(descr, atoi(argv[2]), callback, NULL);
+
+    printf("\nDone sniffing!\n");
+    return 0;
 }
